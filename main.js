7487897c1b7fd28f0e530d6bae4c04b54bef64bf
Sravan Kumar <sravan.iitk.2010@gmail.com>
2025-02-28 11:44:30 +0530

    Functions module task 2 added

---
+++ main.js
@@ -0,0 +1,17 @@
+function add (x,y){
+    return x + y;
+}
+
+const subract = function (x,y){
+    return x - y;
+}
+
+const multiply = (x,y) => x * y;
+
+const divide = (x,y) => y === 0 ? 'undefined' : x/y;
+
+function calculate (x,y){
+    return output = divide(x,y);  
+}
+
+console.log(calculate(10,2));
