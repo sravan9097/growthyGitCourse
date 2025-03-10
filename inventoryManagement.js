45ba8492f295b8cbbd664f82266e4f4fe2af77e6
Sravan Kumar <sravan.iitk.2010@gmail.com>
2025-03-10 11:17:07 +0530

    second  Hands-onActivity of Arrays&Objects  added

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -53,3 +53,18 @@ updateProductQuantity('iphone',10);
 
 removeProduct('macbook');
 console.log("Total Inventory Value:", calTotalValue());
+
+
+// Output elements in reverse order
+
+const arrayNum = [1,2,3,4,5];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
+
+
+// Difference between for..of and for..in
+//for..of: Iterates over values of an iterable (arrays, strings, etc.)
+//for..in: Iterates over keys (indexes or properties) of an object.
+
